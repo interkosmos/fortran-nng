a330d840fb2311dfed393009c0d9673385297332
Philipp <pengel@hs-nb.de>
2026-01-02 00:09:12 +0100

    Add format string.

--- src/nng_macro.c
+++ src/nng_macro.c
@@ -14,32 +14,32 @@ void nng_log_warn_(const char *, const char *);
 
 void nng_log_auth_(nng_log_level level, const char *msgid, const char *msg)
 {
-    nng_log_auth(level, msgid, msg);
+    nng_log_auth(level, msgid, "%s", msg);
 }
 
 void nng_log_debug_(const char *msgid, const char *msg)
 {
-    nng_log_debug(msgid, msg);
+    nng_log_debug(msgid, "%s", msg);
 }
 
 void nng_log_err_(const char *msgid, const char *msg)
 {
-    nng_log_err(msgid, msg);
+    nng_log_err(msgid, "%s", msg);
 }
 
 void nng_log_info_(const char *msgid, const char *msg)
 {
-    nng_log_info(msgid, msg);
+    nng_log_info(msgid, "%s", msg);
 }
 
 void nng_log_notice_(const char *msgid, const char *msg)
 {
-    nng_log_notice(msgid, msg);
+    nng_log_notice(msgid, "%s", msg);
 }
 
 void nng_log_warn_(const char *msgid, const char *msg)
 {
-    nng_log_warn(msgid, msg);
+    nng_log_warn(msgid, "%s", msg);
 }
 
 #ifdef __cplusplus
